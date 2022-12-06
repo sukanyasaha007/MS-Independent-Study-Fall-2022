ebb513dc7f5b899456ee4fc26251a7364e048277
Sukanya <sukanyasaha007@gmail.com>
2022-12-06 01:08:33 -0700

    updated minor commands

--- image._flipping_test.py
+++ image._flipping_test.py
@@ -1,29 +1,20 @@
-
-# Python program to explain cv2.flip() method
+import cv2
  
-import keras
-
-model= keras.models.load_model(r"C:\Users\sahasuka\OneDrive - Intel Corporation\Desktop\Personal\Self-driving car\MS-Independent-Study-Fall-2022\model\model.h5")
-print(model.history.history)
-
-# # importing cv2
-# import cv2
+# path
+path = r"images\track1.jpg"
  
-# # path
-# path = r"C:\Users\sahasuka\OneDrive - Intel Corporation\Desktop\Personal\Self-driving car\MS-Independent-Study-Fall-2022\images\track1.jpg"
+# Reading an image in default mode
+src = cv2.imread(path)
  
-# # Reading an image in default mode
-# src = cv2.imread(path)
+# Window name in which image is displayed
+window_name = 'Image'
  
-# # Window name in which image is displayed
-# window_name = 'Image'
- 
-# # Using cv2.flip() method
-# # Use Flip code 0 to flip vertically
-# image = cv2.flip(src, 2)
+# Using cv2.flip() method
+# Use Flip code 0 to flip vertically
+image = cv2.flip(src, 2)
 
-# # Displaying the image
-# # cv2.imshow(window_name, image)
-# # cv2.waitKey(0)
+# Displaying the image
+# cv2.imshow(window_name, image)
+# cv2.waitKey(0)
 
-# cv2.imwrite(r'C:\Users\sahasuka\OneDrive - Intel Corporation\Desktop\Personal\Self-driving car\MS-Independent-Study-Fall-2022\images\track1_flipped.PNG', image)
+cv2.imwrite(r'images\track1_flipped.PNG', image)
